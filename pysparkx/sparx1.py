6429adba4aa88db735c2c9b3b38d1ab7bf0c4586
MaheshPanchajanya <maheshmax2040@gmail.com>
2022-12-23 19:36:30 +0530

    imputers

--- pysparkx/sparx1.py
+++ pysparkx/sparx1.py
@@ -3,18 +3,52 @@ from pyspark.sql import SparkSession
 session = SparkSession.builder.appName("testapp").getOrCreate()
 
 
-df = session.read.option('header','true').csv('pract1.csv').show()
+df = session.read.csv('pract1.csv',header=True,inferSchema=True)
 
 print(df)
 
-df = session.read.option('header','true').csv('pract1.csv',inferSchema=True).printSchema()
+a = df.printSchema()
 
-print(df)
+print(a)
 
-df = session.read.option('header','true').csv('pract1.csv',inferSchema=True).describe().show()
+b = df.describe().show()
 
-print(df)   
+print(b)
 
-df = session.read.option('header','true').csv('pract1.csv',inferSchema=True).select(['name','age']).show()
+c = df.select(['name','age']).show()
 
-print(df)
+print(c)
+
+d = df.withColumn('exp after 2 years',df['exp']+2).show()
+
+print(d)
+
+e = df.withColumnRenamed('designation','Role').show()
+
+print(e)
+
+x = df.na.drop().show()
+
+print(x)
+
+z = df.na.drop(thresh=3).show()
+
+print(z)
+
+f = df.na.drop(subset='age').show()
+
+print(z)
+
+g = df.na.fill('missing',['age','exp','salary']).show()
+
+print(g)
+
+from pyspark.ml.feature import Imputer
+
+imputer = Imputer(
+    inputCols=['Age','exp','salary'],
+    outputCols=["{}_imputed".format(c) for c in ['Age','exp','salary']]
+).setStrategy('mean')
+
+trnsfrm= imputer.fit(df).transform(df).show()
+print(trnsfrm)
